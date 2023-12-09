eb35cdcbb68ef08777fae0cc65ceff55757b9321
romanyukalexandr84 <romanyukalexandr84@gmail.com>
2023-12-09 03:32:24 +0300

    add readme file and entrypoint

---
+++ Start.java
@@ -0,0 +1,11 @@
+import static Controller.Controller.*;
+
+import java.io.*;
+import java.text.*;
+
+public class Start {
+    public static void main(String[] args) throws IOException, ParseException {
+        fillAnimalsList();
+        workWithMenu();
+    }
+}
